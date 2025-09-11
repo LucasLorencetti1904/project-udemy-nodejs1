4469358a2b511f2400e0ea1cb9c5a04ef3eda0f4
LucasLorencetti1904 <lucaslorencetti222@outlook.com>
2025-09-11 18:09:41 -0300

    feat: adicionar abstracao generica de repository comum

---
+++ src/common/domain/repositories/Repository.ts
@@ -0,0 +1,21 @@
+export type SearchInput = {
+    page?: number,
+    perPage?: number,
+    sort?: string | null,
+    sortDir?: string | null,
+    filter?: string | null
+};
+
+export type SearchOutput<Model> = Required<SearchInput> & {
+    items: Model[],
+    total: number,
+};
+
+export default interface Repository<Model, CreateData> {
+    create(data: CreateData): Model;
+    insert(model: Model): Promise<Model>;
+    findById(id: string): Promise<Model>;
+    update(model: Model): Promise<Model>;
+    delete(id: string): Promise<void>;
+    search(config: SearchInput): Promise<SearchOutput<Model>>;
+}
