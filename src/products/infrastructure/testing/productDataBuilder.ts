e2e99cb4444fc313bc943a92ee1ee2e25850f441
LucasLorencetti1904 <lucaslorencetti222@outlook.com>
2025-10-19 16:49:47 -0300

    feat: desenvolver helper de building de produtos fakes para testes

---
+++ src/products/infrastructure/testing/productDataBuilder.ts
@@ -0,0 +1,14 @@
+import { faker } from "@faker-js/faker";
+import { randomUUID } from "node:crypto";
+import ProductModel from "@/products/domain/models/ProductModel";
+
+export default function productDataBuilder(props: Partial<ProductModel>): ProductModel {
+    return {
+        id: props.id ?? randomUUID(),
+        name: props.name ?? faker.commerce.productName(),
+        price: props.price ?? Number(faker.commerce.price({ min: 100, max: 2000, dec: 2 })),
+        quantity: props.quantity ?? faker.number.int({ min: 1 }),
+        createdAt: props.createdAt ?? new Date(),
+        updatedAt: props.updatedAt ?? new Date()
+    };
+}
