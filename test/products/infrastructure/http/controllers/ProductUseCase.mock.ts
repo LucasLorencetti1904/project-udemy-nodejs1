0f82c2d92174a7115dd043666c37b3329d363581
LucasLorencetti1904 <lucaslorencetti222@outlook.com>
2025-10-31 12:17:18 -0300

    test: fazer teste unitario do UpdateProductController

--- test/products/infrastructure/http/controllers/ProductUseCase.mock.ts
+++ test/products/infrastructure/http/controllers/ProductUseCase.mock.ts
@@ -1,10 +1,15 @@
 import CreateProductUseCase from "@/products/application/usecases/createProduct/CreateProductUseCase";
 import GetProductByIdUseCase from "@/products/application/usecases/getProductById/GetProductByIdUseCase";
+import UpdateProductUseCase from "@/products/application/usecases/updateProduct/UpdateProductUseCase";
 
 export class MockCreateProductUseCase implements CreateProductUseCase {
     public execute = vi.fn();
 }
 
 export class MockGetProductByIdUseCase implements GetProductByIdUseCase {
+    public execute = vi.fn();
+}
+
+export class MockUpdateProductUseCase implements UpdateProductUseCase {
     public execute = vi.fn();
 }
--- test/products/infrastructure/http/controllers/UpdateProductController.test.ts
+++ test/products/infrastructure/http/controllers/UpdateProductController.test.ts
@@ -1,23 +1,22 @@
-import CreateProductController from "@/products/infrastructure/http/controllers/CreateProductController";
-import { MockCreateProductUseCase } from "./ProductUseCase.mock";
+import { MockUpdateProductUseCase } from "./ProductUseCase.mock";
 import { Request, Response } from "express";
-import { ConflictError, InternalError } from "@/common/domain/errors/httpErrors";
+import { ConflictError, InternalError, NotFoundError } from "@/common/domain/errors/httpErrors";
 import productOutputBuilder from "@/products/infrastructure/testing/productOutputBuilder";
-import { createProductInputBuilder } from "@/products/infrastructure/testing/productInputBuilder";
+import { updateProductInputBuilder } from "@/products/infrastructure/testing/productInputBuilder";
 import type ProductOutput from "@/products/application/usecases/default/ProductOutput";
 
-let sut: CreateProductController;
-let mockUseCase: MockCreateProductUseCase;
+let sut: UpdateProductController;
+let mockUseCase: MockUpdateProductUseCase;
 
 let req: Partial<Request>;
 let res: Partial<Response>;
 
-describe ("CreateProductController Test.", () => {
+describe ("UpdateProductController Test.", () => {
     beforeEach (() => {
-        mockUseCase = new MockCreateProductUseCase();
-        sut = new CreateProductController(mockUseCase);
+        mockUseCase = new MockUpdateProductUseCase();
+        sut = new UpdateProductController(mockUseCase);
         req = {
-            body: createProductInputBuilder({})
+            body: updateProductInputBuilder({})
         };
         res = {
             status: vi.fn().mockReturnThis(),
@@ -25,9 +24,9 @@ describe ("CreateProductController Test.", () => {
         };
     });
     
-    [["name", ""], ["quantity", -2], ["price", 0.0]].forEach(([field, value]) => {
+    [["id", ""], ["quantity", -2], ["price", 0.0]].forEach(([field, value]) => {
         it (`should return a response error with code 400 when product ${field} is invalid.`, async () => {
-            req.body = createProductInputBuilder({ [field]: value });
+            req.body = updateProductInputBuilder({ [field]: value });
             await sut.handle(req as Request, res as Response);
             expect (mockUseCase.execute).not.toHaveBeenCalled();
             expect (res.status).toHaveBeenCalledExactlyOnceWith(400);
@@ -35,27 +34,36 @@ describe ("CreateProductController Test.", () => {
         });
     });
 
+    ["name", "quantity", "price"].forEach(([field]) => {
+        it (`should not return a response error with code 400 when product ${field} is undefined.`, async () => {
+            req.body = updateProductInputBuilder({ [field]: undefined });
+            await sut.handle(req as Request, res as Response);
+            expect (mockUseCase.execute).toHaveBeenCalledExactlyOnceWith(req.body);
+            expect (res.status).not.toHaveBeenCalledWith(400);
+            expect (res.json).toHaveBeenCalledOnce();
+        });
+    });
+
     [
         { useCaseError: new InternalError("Example"), statusCode: 500, occasion: "usecase throws an unexpected error" },
+        { useCaseError: new NotFoundError("Example"), status: 404, occasion: "product is not found by id" },
         { useCaseError: new ConflictError("Example"), statusCode: 409, occasion: "product name already exists" }
     ].forEach(({ useCaseError, statusCode, occasion }) => {
         it (`should return a response error with code ${statusCode} when ${occasion}.`, async () => {
-            req.body = createProductInputBuilder({});
             mockUseCase.execute.mockRejectedValue(useCaseError);
             await sut.handle(req as Request, res as Response);
-            expect (mockUseCase.execute).toHaveBeenCalledExactlyOnceWith(req.body);
+            expect (mockUseCase.execute).toHaveBeenCalledWith(req.body);
             expect (res.status).toHaveBeenCalledExactlyOnceWith(statusCode);
             expect (res.json).toHaveBeenCalledExactlyOnceWith({ message: expect.stringContaining("") });
         });
     });
 
-    it (`should return a response product json object with code 201 when product registered successfully.`, async () => {
-        req.body = createProductInputBuilder({});
+    it (`should return a response product json object with code 200 when product updated successfully.`, async () => {
         const useCaseOutput: ProductOutput = productOutputBuilder({ ...req.body });
         mockUseCase.execute.mockResolvedValue(useCaseOutput);
         await sut.handle(req as Request, res as Response);
         expect (mockUseCase.execute).toHaveBeenCalledExactlyOnceWith(req.body);
-        expect (res.status).toHaveBeenCalledExactlyOnceWith(201);
+        expect (res.status).toHaveBeenCalledExactlyOnceWith(200);
         expect (res.json).toHaveBeenCalledExactlyOnceWith({
             message: expect.stringContaining(""), data: useCaseOutput
         });
