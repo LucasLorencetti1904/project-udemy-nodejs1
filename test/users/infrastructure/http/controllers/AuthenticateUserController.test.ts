e660cc5a522b62daff124c0a6063e3a51711710d
LucasLorencetti1904 <lucaslorencetti222@outlook.com>
2025-11-08 17:37:03 -0300

    test: fazer teste unitario do AuthenticateUserController

---
+++ test/users/infrastructure/http/controllers/AuthenticateUserController.test.ts
@@ -0,0 +1,99 @@
+import { MockAuthenticateUserUseCase } from "./UserUseCase.mock";
+import type { UserOutput } from "@/users/application/dto/userIo";
+import { authenticateUserInputBuilder } from "@/users/infrastructure/testing/userInputBuilder";
+import userOutputBuilder from "@/users/infrastructure/testing/userOutputBuilder";
+import type { Request, Response } from "express";
+import { BadRequestError, ConflictError, InternalError } from "@/common/domain/errors/httpErrors";
+
+let sut: AuthenticateUserController;
+let mockUseCase: MockAuthenticateUserUseCase;
+
+let req: Partial<Request>;
+let res: Partial<Response>;
+
+describe ("CreateUserController Test.", () => {
+    beforeEach (() => {
+        mockUseCase = new MockAuthenticateUserUseCase();
+        sut = new AuthenticateUserController(mockUseCase);
+
+        req = {
+            body: authenticateUserInputBuilder({})
+        };
+
+        res = {
+            status: vi.fn().mockReturnThis(),
+            json: vi.fn()
+        };
+    });
+    
+    ["email", "password"].forEach((field) => {
+        it (`should return a response error with code 400 when user ${field} is empty.`, async () => {
+            req.body = authenticateUserInputBuilder({ [field]: "" });
+
+            await sut.handle(req as Request, res as Response);
+
+            expect (mockUseCase.execute).not.toHaveBeenCalled();
+            expect (res.status).toHaveBeenCalledExactlyOnceWith(400);
+            expect (res.json).toHaveBeenCalledExactlyOnceWith({ message: expect.stringContaining("") });
+        });
+    });
+
+    ["invalidEmail", "invalid email @gmail.com", "invalidemailgmail.com", "invalidemail@.com"].forEach((value) => {
+        it (`should return a response error with code 400 when user email is invalid.`, async () => {
+            req.body = authenticateUserInputBuilder({ email: value });
+
+            await sut.handle(req as Request, res as Response);
+
+            expect (mockUseCase.execute).not.toHaveBeenCalled();
+            expect (res.status).toHaveBeenCalledExactlyOnceWith(400);
+            expect (res.json).toHaveBeenCalledExactlyOnceWith({ message: expect.stringContaining("") });
+        });
+    });
+
+    it ("should return a response error with code 400 when user credentials contains invalid fields.", async () => {
+        req.body = { ...authenticateUserInputBuilder({}), unexpectedField: "Unexpected Value" };
+
+        await sut.handle(req as Request, res as Response);
+
+        expect (mockUseCase.execute).not.toHaveBeenCalled();
+        expect (res.status).toHaveBeenCalledExactlyOnceWith(400);
+        expect (res.json).toHaveBeenCalledExactlyOnceWith({ message: expect.stringContaining("") });
+    });
+
+    [
+        { useCaseError: new InternalError("Example"), statusCode: 500, occasion: "usecase throws an unexpected error" },
+        { useCaseError: new ConflictError("Example"), statusCode: 409, occasion: "user email already exists" },
+        { useCaseError: new BadRequestError("Example"), statusCode: 409, occasion: "user password does not match" }
+    ].forEach(({ useCaseError, statusCode, occasion }) => {
+        it (`should return a response error with code ${statusCode} when ${occasion}.`, async () => {
+            mockUseCase.execute.mockRejectedValue(useCaseError);
+
+            await sut.handle(req as Request, res as Response);
+
+            expect (mockUseCase.execute).toHaveBeenCalledExactlyOnceWith(req.body);
+            expect (res.status).toHaveBeenCalledExactlyOnceWith(statusCode);
+            expect (res.json).toHaveBeenCalledExactlyOnceWith({ message: expect.stringContaining("") });
+        });
+    });
+
+
+    [
+        { email: "emailexample@gmail.com" }, { email: "exampleofemail@hotmail.com" },
+        { password: "123456789a" }, { password: "UserPasswordExample123!*" }
+    ]
+    .forEach((specificInput) => {
+        it (`should return a response user (without password) json object with code 201 when user registered successfully.`, async () => {
+            req.body = authenticateUserInputBuilder({ ...specificInput });
+            const useCaseOutput: UserOutput = userOutputBuilder({ ...req.body });
+            mockUseCase.execute.mockResolvedValue(useCaseOutput);
+
+            await sut.handle(req as Request, res as Response);
+
+            expect (mockUseCase.execute).toHaveBeenCalledExactlyOnceWith(req.body);
+            expect (res.status).toHaveBeenCalledExactlyOnceWith(201);
+            expect (res.json).toHaveBeenCalledExactlyOnceWith({
+                message: expect.stringContaining(""), data: useCaseOutput
+            });
+        });
+    });
+});
--- test/users/infrastructure/http/controllers/UserUseCase.mock.ts
+++ test/users/infrastructure/http/controllers/UserUseCase.mock.ts
@@ -1,5 +1,10 @@
+import type AuthenticateUserUseCase from "@/users/application/usecases/authenticateUser/AuthenticateUserUseCase";
 import type CreateUserUseCase from "@/users/application/usecases/createUser/CreateUserUseCase";
 import type SearchUserUseCase from "@/users/application/usecases/searchUser/SearchUserUseCase";
+
+export class MockAuthenticateUserUseCase implements AuthenticateUserUseCase {
+    public execute = vi.fn();
+}
 
 export class MockCreateUserUseCase implements CreateUserUseCase {
     public execute = vi.fn();
